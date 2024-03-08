66fa3409ce68c98dd5a7f7f0775d5a2f2d8f0c30
MikaelKevintanNaibaho <mikaelkevintannaibaho@gmail.com>
2024-03-08 20:35:38 +0700

    fix : try to fixing the calibaration

--- main.c
+++ main.c
@@ -17,14 +17,25 @@ int main(void){
         return 1;
     }
 
-    AllSensorData all_data = cmps12_read_all_data(cmps12_file);
+    uint8_t calibration_status = cmps12_read_calibration_state(cmps12_file);
+    if (calibration_status == 0x03) {
+        printf("CMPS12 sensor fully calibrated\n");
+    } else {
+        printf("CMPS12 sensor calibration in progress\n");
+    }
+    while (1){
+        AllSensorData all_data = cmps12_read_all_data(cmps12_file);
 
-    // Print the data
-    printf("Magnetometer: X=%u, Y=%u, Z=%u\n", all_data.magnetometer.x, all_data.magnetometer.y, all_data.magnetometer.z);
-    printf("Accelerometer: X=%u, Y=%u, Z=%u\n", all_data.accelerometer.x, all_data.accelerometer.y, all_data.accelerometer.z);
-    printf("Gyroscope: X=%u, Y=%u, Z=%u\n", all_data.gyro.x, all_data.gyro.y, all_data.gyro.z);
-    printf("Orientation: Bearing=%u, Pitch=%u, Roll=%u\n", all_data.orientation.bearing, all_data.orientation.pitch, all_data.orientation.roll);
+        // Print the data
+        printf("Magnetometer: X=%u, Y=%u, Z=%u\n", all_data.magnetometer.x, all_data.magnetometer.y, all_data.magnetometer.z);
+        printf("Accelerometer: X=%u, Y=%u, Z=%u\n", all_data.accelerometer.x, all_data.accelerometer.y, all_data.accelerometer.z);
+        printf("Gyroscope: X=%u, Y=%u, Z=%u\n", all_data.gyro.x, all_data.gyro.y, all_data.gyro.z);
+        printf("Orientation: Bearing=%u, Pitch=%u, Roll=%u\n", all_data.orientation.bearing, all_data.orientation.pitch, all_data.orientation.roll);
 
+        usleep(1000000); //delay 1 second
+
+    }
+    
 
     cmps12_release_i2c_device(cmps12_file);
 
